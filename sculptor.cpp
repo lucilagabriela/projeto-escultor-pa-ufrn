9a3e3a14a41c6d8fcc0b4378f07a73cf1090472a
Lucila Costa <lucilagdac@gmail.com>
2023-09-25 10:19:27 -0300

    up

---
+++ sculptor.cpp
@@ -0,0 +1,17 @@
+// 1) construtor -- Voxel ***v; alocar "v"
+// 2) destrutor -- liberar "v"
+// 3) setColor()
+// 4) putVoxel()
+// 5) writeOFF()
+
+#include "sculptor.h"
+
+Sculptor::Sculptor(int _nx, int _ny, int _nz) {
+    nx = _nx;
+    ny = _ny;
+    nz = _nz;
+}
+
+Sculptor:: ~Sculptor() {
+    
+}
